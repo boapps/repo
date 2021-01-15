7b1a729b2c6ecb981c47720411789a7a7d643967
boa <boa@pm.me>
2021-01-15 22:24:06 +0100

    test

---
+++ config.py
@@ -0,0 +1,8 @@
+
+repo_keyalias = "boa-laptop"
+
+keydname = "CN=boa-laptop, OU=F-Droid"
+
+keystorepass = "p4ISBj/Ls979TjDy0I2nYLcmW2/IIJNZeXtAvPFKKIw="
+
+keypass = "p4ISBj/Ls979TjDy0I2nYLcmW2/IIJNZeXtAvPFKKIw="
